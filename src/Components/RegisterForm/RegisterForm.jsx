fb31fc3f8affb8f7ac8836d212d31a1f990e1bd1
Facundo-Oliva <facundooliva500@hotmail.com>
2024-04-04 11:12:00 -0300

    Register basic

--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -1,9 +1,31 @@
 import "./registerForm.css"
 
-
 const RegisterForm = () => {
     return (
-        <h1>RegisterForm</h1>
+        <>
+            <div className="register-container">
+                <h3 className="text-center">Register</h3>
+                <form className="d-flex flex-column align-items-center">
+                    <div className="group-input ">
+                        <label htmlFor="user">Nombre de Usuario</label>
+                        <input type="text" id="user"  className="form-control"/>
+                    </div>
+                    <div className="group-input">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email"  className="form-control"/>
+                    </div>
+                    <div className="group-input">
+                        <label htmlFor="birth-date">Fecha de Nacimiento</label>
+                        <input type="date" id="birth-date" className="form-control"/>
+                    </div>
+                    <div className="group-input">
+                        <label htmlFor="password">Contraseña</label>
+                        <input type="password" id="password" className="form-control"/>
+                    </div>
+                    <button type="submit" className="btn btn-outline-success btn-create">Crear Cuenta</button>
+                </form>
+            </div>
+        </>
     )
 }
 
