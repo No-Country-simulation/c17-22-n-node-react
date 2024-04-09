962eebcef57bb430c088066b85f6e5178c2b88d5
Facundo-Oliva <facundooliva500@hotmail.com>
2024-04-09 09:59:12 -0300

    register and login

--- src/Components/RegisterForm/RegisterForm.jsx
+++ src/Components/RegisterForm/RegisterForm.jsx
@@ -7,30 +7,34 @@ const RegisterForm = () => {
                 <h3 className="text-center">Registro</h3>
                 <form className="d-flex flex-column align-items-center">
                     <div className="group-input ">
-                        <label htmlFor="user">Nombre de Usuario</label>
+                        <label htmlFor="user">Nombre de Usuario:</label>
                         <input type="text" id="user"  className="form-control"/>
                     </div>
                     <div className="group-input">
-                        <label htmlFor="email">Email</label>
+                        <label htmlFor="email">Email:</label>
                         <input type="email" id="email"  className="form-control"/>
                     </div>
                     <div className="group-input">
-                        <label htmlFor="birth-date">Fecha de Nacimiento</label>
+                        <label htmlFor="birth-date">Fecha de Nacimiento:</label>
                         <input type="date" id="birth-date" className="form-control"/>
                     </div>
                     <div className="group-input">
-                        <label htmlFor="password">Contraseña</label>
+                        <label htmlFor="password">Contraseña:</label>
                         <input type="password" id="password" className="form-control"/>
                     </div>
                     <div className="group-input">
-                        <label htmlFor="confirmPassword">Confirmar contraseña</label>
+                        <label htmlFor="confirmPassword">Confirmar contraseña:</label>
                         <input type="password" id="confirmPassword" className="form-control"/>
                     </div>
-                    <div className="group-input">
-                        <label htmlFor="">Emprendedor</label>
-                        <input type="radio" name="typeOfAcount" />
-                        <label htmlFor="">Inversor</label>
-                        <input type="radio" name="typeOfAcount" />
+                    <div className="group-input radio-container">
+                        <label htmlFor="typeOfAcount" className="d-flex align-items-center gap-2">
+                            <input type="radio" name="typeOfAcount" />
+                            <span>Emprendedor</span>
+                        </label>
+                        <label htmlFor="typeOfAcount" className="d-flex align-items-center gap-2">
+                            <input type="radio" name="typeOfAcount" />
+                            <span>Inversor</span>
+                        </label>
                     </div>
                     <button type="submit" className="btn btn-outline-success btn-create">Crear Cuenta</button>
                 </form>
