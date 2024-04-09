b3281bb75d8ffe27f727c8a85ed6795af1478f28
Agustin <agusscaramello@gmail.com>
2024-04-09 11:51:35 -0300

    fix/redux

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer/reducer";
 
-export const store = createStore( rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
