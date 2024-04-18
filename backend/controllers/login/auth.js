bf2fefb5816f3c53962f610df62aa7054de5c4f7
Paul_Luque <pluquec@unsa.edu.pe>
2024-04-17 20:18:55 -0500

    add : register controller

--- backend/controllers/login/auth.js
+++ backend/controllers/login/auth.js
@@ -1,4 +1,4 @@
-const { encrypt, compare } = require('../../helpers/handleBcrypt')
+const { compare } = require('../../helpers/handleBcrypt')
 const { tokenSign } = require('../../helpers/generateTokens')
 const { PrismaClient } = require('@prisma/client')
 
@@ -53,11 +53,32 @@ const loginCtrl = async (req, res) => {
 
   } catch (e) {
     res.status(404)
-    res.send({ error: 'User or password not found',
-  e })
+    res.send({ error: 'User or password not found' })
   }
 }
 
+const registerCtrl = async (req, res) => {
+  try {
+    const { username, city, country, password, email, investor, url, imageUrl } = req.body
 
+    // const passwordHash = await encrypt(password) //TODO: (123456)<--- Encriptando!!
+    const registerUser = await prismaClient.user.create({
+      data: { 
+        name: username,
+        email: email, 
+        city: city ,
+        country: country,
+        password: password,
+      }
 
-module.exports = { loginCtrl }
+    })
+
+    res.send({ data: registerUser })
+
+  } catch (e) {
+    res.status(404)
+    res.send({ error: 'Invalidad data'})
+  }
+}
+
+module.exports = { loginCtrl, registerCtrl }
