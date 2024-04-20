bf9641284a47e3957453fae22933a487ccbc379d
lpolverino <lorenz.polverino@gmail.com>
2024-04-20 10:54:33 -0300

    Added token validators

---
+++ backend/helpers/tokenValidators.js
@@ -0,0 +1,82 @@
+const jwt = require("jsonwebtoken");
+import { jwtDecode } from "jwt-decode";
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const parseToken = (req,res,next) =>{
+  const bearerHeader = req.headers['authorization']
+  if(typeof bearerHeader !== 'undefined'){
+    const bearer = bearerHeader.split(' ');
+    const bearerToken = bearer[1]
+    req.token = bearerToken;
+    next()
+  } else{
+    res.status(403).json({msg:"Invalid Authorizations Header"})
+  }
+}
+
+
+const isValidToken = (req,res,next) => {
+  jwt.verify(req.token, process.env.JWT_SECRET, (err, authData) => {
+    if(err){
+      return res.status(403).json({msg:`Validation Fail`})
+    }else{
+     next() 
+    }
+  })
+}
+
+const isValidEntrepreneur = async (req,res, next) => {
+  const decodedToken = jwtDecode(req.token)
+  const result = await prisma.entrepeneur.findFirst({
+    where:{user:decodedToken.userId}
+  })
+  if(result){
+    next()
+  } else{
+    return res.status(403).json({msg:"Precondition Failed"});
+  }
+}
+
+const isValidInvestor = async (req, res, next) => {
+ const decodedToken = jwtDecode(req.token)
+  const result = await prisma.investor.findFirst({
+    where:{user:decodedToken.userId}
+  })
+  if(result){
+    next()
+  } else{
+    return res.status(403).json({msg:"Precondition Failed"});
+  }
+}
+
+const isAdmin = async (req, res, next) => {
+  const decodedToken = jwtDecode(req.token);
+  const responses = await Promise.all([
+    prisma.user.findFirst({
+      where:{id:decodedToken.userId}, data:{id},
+    }),
+    prisma.investor.findFirst({
+      where:{user: decodedToken.userId}, data:{id},
+    }),
+    prisma.entrepeneur.findFirst({
+      where:{user: decodedToken.userId}, data:{id},
+    })
+  ])
+
+  if(responses[0] !== null && responses[1] === null && responses[2] === null){
+    next()
+  } else{
+    return res.status(403).json({msg:"Forbibben"});
+  }
+}
+
+module.exports = {
+  parseToken,
+  isValidToken,
+  isValidEntrepreneur,
+  isValidInvestor,
+  isAdmin,
+}
+
--- backend/routes/entrepreneurships.js
+++ backend/routes/entrepreneurships.js
@@ -1,19 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/entrepreneurship");
-
+const authentication = require("../helpers/tokenValidators");
 // const authentication = require("")
 
 
 const dummy = (req,res,next) => {
   res.send('respond with a resource');
-}
-
-const authentication = {
-  isValidToken: dummy,
-  isValidEntrepreneur: dummy,
-  isValidInvestor: dummy,
-  isAdmin: dummy,
 }
 
 /* GET users listing. */
@@ -22,14 +15,14 @@ router.get('/:entrepreneurshipId', dummy);
 router.get('/:entrepreneurshipId/vote', dummy);
 router.get('/:entrepreneurshipId/vote/:voteId', dummy);
 
-router.post('/', authentication.isValidToken, authentication.isValidEntrepreneur,dummy);
-router.post('/vote', authentication.isValidToken, authentication.isValidInvestor, dummy);
+router.post('/', authentication.parseToken, authentication.isValidToken, authentication.isValidEntrepreneur,dummy);
+router.post('/vote',authentication.parseToken,  authentication.isValidToken, authentication.isValidInvestor, dummy);
 
-router.put('/:entrepreneurshipId', authentication.isValidToken, authentication.isValidEntrepreneur,dummy);
+router.put('/:entrepreneurshipId',authentication.parseToken,  authentication.isValidToken, authentication.isValidEntrepreneur,dummy);
 router.put('/:entrepreneurshipId/vote/:voteId', authentication.isValidToken, authentication.isValidInvestor,dummy);
 
-router.delete('/:entrepreneurshipId',authentication.isValidToken, authentication.isValidEntrepreneur, dummy)
-router.delete('/:entrepreneurshipId/vote', authentication.isValidToken, authentication.isAdmin, dummy)
-router.delete('/:entrepreneurshipId/vote/:voteId',authentication.isValidToken, authentication.isValidInvestor,dummy)
+router.delete('/:entrepreneurshipId',authentication.parseToken, authentication.isValidToken, authentication.isValidEntrepreneur, dummy)
+router.delete('/:entrepreneurshipId/vote',authentication.parseToken,  authentication.isValidToken, authentication.isAdmin, dummy)
+router.delete('/:entrepreneurshipId/vote/:voteId',authentication.parseToken, authentication.isValidToken, authentication.isValidInvestor,dummy)
 
 module.exports = router;
