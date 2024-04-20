4ce46a370c3f6760d89cc1f5a573a4848862dbd9
Paul_Luque <pluquec@unsa.edu.pe>
2024-04-19 19:29:34 -0500

    add: improved register

--- backend/controllers/login/auth.js
+++ backend/controllers/login/auth.js
@@ -60,25 +60,61 @@ const loginCtrl = async (req, res) => {
 
 const registerCtrl = async (req, res) => {
   try {
-    const { username, city, country, password, email, investor, url, imageUrl } = req.body
+    const { username, email, city, country, password, investor, url, imageUrl } = req.body
 
-    // const passwordHash = await encrypt(password) //TODO: (123456)<--- Encriptando!!
+    // check if user is already registered
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email: username
+      }
+    })
+
+    if (user) {
+      res.status(409)
+      res.send({ error: 'User already exist' })
+      return
+    }
+
+    /// username deberia de ser name , en el excel
     const registerUser = await prismaClient.user.create({
-      data: { 
+      data: {
         name: username,
-        email: email, 
-        city: city ,
+        email: email,
+        city: city,
         country: country,
         password: password,
       }
 
     })
 
-    res.send({ data: registerUser })
+    if (investor) {
+      const investor = await prismaClient.investor.create({
+        data: {
+          id: registerUser.id,
+          mensual_votes: 0
+
+        }
+      })
+    } else {
+      const entrepeneur = await prismaClient.entrepeneur.create({
+        data: {
+          id: registerUser.id,
+          mensual_tickets: 0
+
+        }
+      })
+    }
+
+    res.send({
+      msg: "User created succefully",
+      id: registerUser.id,
+      name: registerUser.name,
+      email: registerUser.email
+    })
 
   } catch (e) {
     res.status(404)
-    res.send({ error: 'Invalidad data'})
+    res.send({ error: 'Invalidad data' })
   }
 }
 
