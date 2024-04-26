ba9f92028c8c96c1ab1c40078c166f3db88fe419
Agustin <agusscaramello@gmail.com>
2024-04-26 04:54:36 -0300

    conectandoRedux

--- Frontend/src/Components/EditProfile/EditProfile.jsx
+++ Frontend/src/Components/EditProfile/EditProfile.jsx
@@ -1,168 +1,168 @@
-import "./EditProfile.css";
-import { useEffect, useState } from "react";
-import { uploadImg } from "../../utils/uploadImg";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
-import { getUserById, updateUser } from "../../redux/actions/userActions";
-import Error404 from "../Error404/Error404";
-import cargarImagen from "../../assets/img/cargarImagen.jpg";
+import "./EditProfile.css"
+import { useEffect, useState } from "react"
+import { uploadImg } from "../../utils/uploadImg"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate, useParams } from "react-router-dom"
+import { getUserById, updateUser } from "../../redux/actions/userActions"
+import Error404 from "../Error404/Error404"
+import cargarImagen from "../../assets/img/cargarImagen.jpg"
 
 const EditProfile = () => {
-  const { userId } = useParams();
-  const dispatch = useDispatch();
-  const navegation = useNavigate();
-  const isAuth = useSelector((state) => state.userLogged);
+	const { userId } = useParams()
+	const dispatch = useDispatch()
+	const navegation = useNavigate()
+	const isAuth = useSelector((state) => state.userLogged)
 
-  const currentUser = useSelector((state) =>
-    state.users.find((u) => u.userId === userId)
-  );
+	const token = useSelector((state) => state.token)
 
-  const [profile, setProfile] = useState({
-    username: "",
-    email: "",
-    password: "",
-    image: "",
-    url: "",
-  });
+	const currentUser = useSelector((state) =>
+		state.users.find((u) => u.userId === userId)
+	)
 
-  useEffect(() => {
-    dispatch(getUserById(userId));
-  }, [dispatch, userId]);
+	const [profile, setProfile] = useState({
+		name: "",
+		email: "",
+		imageUrl: "",
+		instagramUrl: "",
+	})
 
-  useEffect(() => {
-    if (currentUser) {
-      setProfile({
-        username: currentUser.username,
-        email: currentUser.email,
-        password: currentUser.password,
-        image: currentUser.image,
-        url: currentUser.url,
-      });
-    }
-  }, [currentUser]);
+	useEffect(() => {
+		dispatch(getUserById(userId))
+	}, [dispatch, userId])
 
-  const handleImageProfile = async (e) => {
-    const imgProfile = await uploadImg(e);
-    setProfile({ ...profile, image: imgProfile });
-  };
+	useEffect(() => {
+		if (currentUser) {
+			setProfile({
+				name: currentUser.name,
+				email: currentUser.email,
+				imageUrl: currentUser.imageUrl,
+				instagramUrl: currentUser.instagramUrl,
+			})
+		}
+	}, [currentUser])
 
-  const handleDeleteImage = () => {
-    setProfile({ ...profile, image: "" });
-  };
+	const handleImageProfile = async (e) => {
+		const imgProfile = await uploadImg(e)
+		setProfile({ ...profile, imageUrl: imgProfile })
+	}
 
-  const handleChangeProfile = (e) => {
-    const { name, value } = e.target;
-    setProfile((prev) => ({ ...prev, [name]: value }));
-  };
+	const handleDeleteImage = () => {
+		setProfile({ ...profile, imageUrl: "" })
+	}
 
-  const handleProfile = () => {
-    dispatch(updateUser(profile));
-  };
+	const handleChangeProfile = (e) => {
+		const { name, value } = e.target
+		setProfile({ ...profile, [name]: value })
+	}
 
-  const deleteUser = () => {
-    dispatch(deleteUser(userId)).then(navegation("/"));
-  };
+	const handleProfile = () => {
+		dispatch(updateUser(userId, profile, token))
+	}
 
-  if (!currentUser && !isAuth) {
-    return <Error404 />;
-  }
+	const deleteUser = () => {
+		dispatch(deleteUser(userId, token)).then(navegation("/"))
+	}
 
-  return (
-    <>
-      <div className="edit-profile-container bg-body-tertiary">
-        <h3 className="text-center">Editar Perfil</h3>
-        <form className="profile-form">
-          <div>
-            <div className="image-container-profile">
-              {profile.image ? (
-                <img
-                  src={profile.image}
-                  className="show-img-profile"
-                  alt="Imagen seleccionada"
-                />
-              ) : (
-                <img
-                  src={cargarImagen}
-                  className="show-img-profile"
-                  alt="Imagen seleccionada"
-                />
-              )}
-              {profile.image && (
-                <button onClick={handleDeleteImage} className="btn btn-danger">
-                  Eliminar Imagen
-                </button>
-              )}
-            </div>
-            {!profile.image && (
-              <div
-                className="group-input-profile"
-                style={{ height: "2.45rem" }}
-              >
-                <input
-                  type="file"
-                  id="img-profile"
-                  name="image"
-                  className="form-control input-profile"
-                  onChange={handleImageProfile}
-                />
-              </div>
-            )}
-          </div>
-          <div className="group-input-profile">
-            <label htmlFor="username">Nombre:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              className="form-control input-profile"
-              placeholder="Nombre"
-              value={profile.username}
-              onChange={handleChangeProfile}
-            />
-          </div>
-          <div className="group-input-profile">
-            <label htmlFor="username-profile">Email:</label>
-            <input
-              type="text"
-              id="username-profile"
-              className="form-control input-profile"
-              name="email"
-              placeholder="Email"
-              value={profile.email}
-              onChange={handleChangeProfile}
-            />
-          </div>
-          <div className="group-input-profile">
-            <label htmlFor="url-profile">Url de Instagram:</label>
-            <input
-              type="text"
-              id="url-profile"
-              className="form-control input-profile"
-              name="url"
-              value={profile.url}
-              placeholder="Url Instagram"
-              onChange={handleChangeProfile}
-            />
-          </div>
+	if (!currentUser && !isAuth) {
+		return <Error404 />
+	}
 
-          <button
-            type="button"
-            className="btn efectoBoton"
-            onClick={() => handleProfile()}
-          >
-            Guardar Cambios
-          </button>
-        </form>
-        <div className="danger-zone">
-          <h4>Zona de peligro!</h4>
-          <p>Tu cuenta se borrara cuando hagas click en el boton</p>
-          <button className="btn btn-danger" onClick={() => deleteUser()}>
-            Borrar Cuenta
-          </button>
-        </div>
-      </div>
-    </>
-  );
-};
+	return (
+		<>
+			<div className="edit-profile-container bg-body-tertiary">
+				<h3 className="text-center">Editar Perfil</h3>
+				<form className="profile-form">
+					<div>
+						<div className="image-container-profile">
+							{profile.imageUrl ? (
+								<img
+									src={profile.imageUrl}
+									className="show-img-profile"
+									alt="Imagen seleccionada"
+								/>
+							) : (
+								<img
+									src={cargarImagen}
+									className="show-img-profile"
+									alt="Imagen seleccionada"
+								/>
+							)}
+							{profile.imageUrl && (
+								<button onClick={handleDeleteImage} className="btn btn-danger">
+									Eliminar Imagen
+								</button>
+							)}
+						</div>
+						{!profile.imageUrl && (
+							<div
+								className="group-input-profile"
+								style={{ height: "2.45rem" }}
+							>
+								<input
+									type="file"
+									id="img-profile"
+									name="imageUrl"
+									className="form-control input-profile"
+									onChange={handleImageProfile}
+								/>
+							</div>
+						)}
+					</div>
+					<div className="group-input-profile">
+						<label htmlFor="name">Nombre:</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							className="form-control input-profile"
+							placeholder="Nombre"
+							value={profile.name}
+							onChange={handleChangeProfile}
+						/>
+					</div>
+					<div className="group-input-profile">
+						<label htmlFor="name-profile">Email:</label>
+						<input
+							type="text"
+							id="name-profile"
+							className="form-control input-profile"
+							name="email"
+							placeholder="Email"
+							value={profile.email}
+							onChange={handleChangeProfile}
+						/>
+					</div>
+					<div className="group-input-profile">
+						<label htmlFor="instagramUrl-profile">Url de Instagram:</label>
+						<input
+							type="text"
+							id="instagramUrl-profile"
+							className="form-control input-profile"
+							name="instagramUrl"
+							value={profile.instagramUrl}
+							placeholder="Url Instagram"
+							onChange={handleChangeProfile}
+						/>
+					</div>
 
-export default EditProfile;
+					<button
+						type="button"
+						className="btn efectoBoton"
+						onClick={() => handleProfile()}
+					>
+						Guardar Cambios
+					</button>
+				</form>
+				<div className="danger-zone">
+					<h4>Zona de peligro!</h4>
+					<p>Tu cuenta se borrara cuando hagas click en el boton</p>
+					<button className="btn btn-danger" onClick={() => deleteUser()}>
+						Borrar Cuenta
+					</button>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default EditProfile
--- Frontend/src/Components/RegisterForm/RegisterForm.jsx
+++ Frontend/src/Components/RegisterForm/RegisterForm.jsx
@@ -1,177 +1,179 @@
-import { NavLink, useNavigate } from "react-router-dom";
-import lg from "../../assets/img/Story Starter.svg";
-import "./registerForm.css";
-import { useDispatch } from "react-redux";
-import { useState } from "react";
-import { addUser } from "../../redux/actions/userActions";
+import { NavLink, useNavigate } from "react-router-dom"
+import lg from "../../assets/img/Story Starter.svg"
+import "./registerForm.css"
+import { useDispatch } from "react-redux"
+import { useState } from "react"
+import { addUser } from "../../redux/actions/userActions"
 
 const RegisterForm = () => {
-  const dispatch = useDispatch();
-  const navegation = useNavigate();
-  const [formRegister, setFormRegister] = useState({
-    user: "",
-    email: "",
-    password: "",
-    passwordRepeat: "",
-    investor: null,
-  });
-  const [errorRegister, setErrorRegister] = useState(null);
+	const dispatch = useDispatch()
+	const navegation = useNavigate()
+	const [formRegister, setFormRegister] = useState({
+		username: "",
+		email: "",
+		password: "",
+		passwordRepeat: "",
+		investor: null,
+	})
+	const [errorRegister, setErrorRegister] = useState(null)
 
-  const handleChangeRegister = (e) => {
-    const { name, value, type } = e.target;
+	const handleChangeRegister = (e) => {
+		const { name, value, type } = e.target
 
-    if (type === "radio") {
-      const radioValue =
-        value === "true" ? true : value === "false" ? false : null;
-      if (radioValue === null) {
-        setErrorRegister("El valor del radio es null");
-      } else {
-        setFormRegister((prev) => ({ ...prev, [name]: radioValue }));
-      }
-    } else {
-      setFormRegister((prev) => ({ ...prev, [name]: value }));
-    }
-  };
+		if (type === "radio") {
+			const radioValue =
+				value === "true" ? true : value === "false" ? false : null
+			if (radioValue === null) {
+				setErrorRegister("El valor del radio es null")
+			} else {
+				setFormRegister((prev) => ({ ...prev, [name]: radioValue }))
+			}
+		} else {
+			setFormRegister((prev) => ({ ...prev, [name]: value }))
+		}
+	}
 
-  const handleRegister = () => {
-    if (
-      !formRegister.user ||
-      !formRegister.email ||
-      !formRegister.password ||
-      !formRegister.passwordRepeat ||
-      formRegister.investor === null ||
-      formRegister.investor === undefined
-    ) {
-      setErrorRegister("Por favor, completa todos los campos.");
-      return;
-    }
+	const handleRegister = () => {
+		if (
+			!formRegister.username ||
+			!formRegister.email ||
+			!formRegister.password ||
+			!formRegister.passwordRepeat ||
+			formRegister.investor === null ||
+			formRegister.investor === undefined
+		) {
+			setErrorRegister("Por favor, completa todos los campos.")
+			return
+		}
 
-    if (formRegister.password !== formRegister.passwordRepeat) {
-      setErrorRegister("La contraseñas no coinciden");
-      return;
-    }
+		if (formRegister.password !== formRegister.passwordRepeat) {
+			setErrorRegister("La contraseñas no coinciden")
+			return
+		}
 
-    /* Con el regex controlo que la contra tenga al menos un digito, una miniscula, una mayuscula, un caracter especial y sea de min de 8 caracteres */
-    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
+		if (formRegister.password < 8) {
+			setErrorRegister("La contraseña debe tener al menos 8 caracteres.")
+			return
+		}
 
-    if (!regex.test(formRegister.password)) {
-      setErrorRegister(
-        "La contraseña debe tener al menos 8 caracteres, incluyendo mayúsculas, minúsculas, números y caracteres especiales."
-      );
-      return;
-    }
+		const newUser = {
+			username: formRegister.username,
+			password: formRegister.password,
+			email: formRegister.email,
+			investor: formRegister.investor,
+		}
 
-    dispatch(addUser(formRegister))
-      .then(() => {
-        navegation("/login");
-      })
-      .catch((err) => {
-        console.log(err);
-        setErrorRegister("Ocurrio un fallo al registrar al usuario");
-      });
-    console.log(errorRegister);
-  };
+		dispatch(addUser(newUser))
+			.then(() => {
+				navegation("/login")
+			})
+			.catch((err) => {
+				console.log(err)
+				setErrorRegister("Ocurrio un fallo al registrar al usuario")
+			})
+		console.log(errorRegister)
+	}
 
-  return (
-    <>
-      <div className="login-container bg-body-tertiary d-flex flex-column align-items-center justify-content-center gap-3">
-        <NavLink to="/" className="navbar-brand">
-          <img src={lg} alt="Logo Register" />
-        </NavLink>
-        <div className="register-container-form">
-          <p className="text-end small">
-            ¿Tines una cuenta?{" "}
-            <NavLink to="/login" style={{ textDecoration: "none" }}>
-              Ingresar
-            </NavLink>
-          </p>
-          <h3 className="text-start pb-3">Registrarme</h3>
-          <form className="d-flex flex-column justify-content-center align-items-center gap-2">
-            <div className="group-input ">
-              <input
-                type="text"
-                id="user"
-                name="user"
-                className="form-control"
-                placeholder="Nombre"
-                onChange={handleChangeRegister}
-              />
-            </div>
-            <div className="group-input">
-              <input
-                type="email"
-                name="email"
-                autoComplete="email"
-                id="email-register"
-                className="form-control"
-                placeholder="Email"
-                onChange={handleChangeRegister}
-              />
-            </div>
-            <div className="group-input">
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="form-control"
-                placeholder="Contraseña"
-                onChange={handleChangeRegister}
-              />
-            </div>
-            <div className="group-input">
-              <input
-                type="password"
-                name="passwordRepeat"
-                id="confirmPassword"
-                className="form-control"
-                placeholder="Repetir contraseña"
-                onChange={handleChangeRegister}
-              />
-            </div>
-            <div className="toggle-container-toa">
-              <p>Quiero ingresar como:</p>
-              <div className="toggle-radio-toa">
-                <input
-                  type="radio"
-                  value="true"
-                  name="investor"
-                  checked={formRegister.investor === true}
-                  id="toggle-toa-investor"
-                  onChange={handleChangeRegister}
-                />
-                <label htmlFor="toggle-toa-investor">Inversor</label>
-                <input
-                  onChange={handleChangeRegister}
-                  value="false"
-                  checked={formRegister.investor === false}
-                  type="radio"
-                  name="investor"
-                  id="toggle-toa-entreprenour"
-                />
-                <label htmlFor="toggle-toa-entreprenour">Emprendedor</label>
-              </div>
-            </div>
-            <button
-              type="button"
-              className="btn btn-create"
-              onClick={() => handleRegister()}
-            >
-              Crear Cuenta
-            </button>
-            <div className="info-links-register">
-              <span>
-                Al registrarte, aceptas nuestra{" "}
-                <a href="#">Política de privacidad</a>,{" "}
-                <a href="#">Politica de cookies</a> y los{" "}
-                <a href="#">Términos de uso</a>
-              </span>
-            </div>
-          </form>
-        </div>
-      </div>
-      <div className="errores-container">{errorRegister}</div>
-    </>
-  );
-};
+	return (
+		<>
+			<div className="login-container bg-body-tertiary d-flex flex-column align-items-center justify-content-center gap-3">
+				<NavLink to="/" className="navbar-brand">
+					<img src={lg} alt="Logo Register" />
+				</NavLink>
+				<div className="register-container-form">
+					<p className="text-end small">
+						¿Tines una cuenta?{" "}
+						<NavLink to="/login" style={{ textDecoration: "none" }}>
+							Ingresar
+						</NavLink>
+					</p>
+					<h3 className="text-start pb-3">Registrarme</h3>
+					<form className="d-flex flex-column justify-content-center align-items-center gap-2">
+						<div className="group-input ">
+							<input
+								type="text"
+								id="username"
+								name="username"
+								className="form-control"
+								placeholder="Nombre"
+								onChange={handleChangeRegister}
+							/>
+						</div>
+						<div className="group-input">
+							<input
+								type="email"
+								name="email"
+								autoComplete="email"
+								id="email-register"
+								className="form-control"
+								placeholder="Email"
+								onChange={handleChangeRegister}
+							/>
+						</div>
+						<div className="group-input">
+							<input
+								type="password"
+								id="password"
+								name="password"
+								className="form-control"
+								placeholder="Contraseña"
+								onChange={handleChangeRegister}
+							/>
+						</div>
+						<div className="group-input">
+							<input
+								type="password"
+								name="passwordRepeat"
+								id="confirmPassword"
+								className="form-control"
+								placeholder="Repetir contraseña"
+								onChange={handleChangeRegister}
+							/>
+						</div>
+						<div className="toggle-container-toa">
+							<p>Quiero ingresar como:</p>
+							<div className="toggle-radio-toa">
+								<input
+									type="radio"
+									value="true"
+									name="investor"
+									checked={formRegister.investor === true}
+									id="toggle-toa-investor"
+									onChange={handleChangeRegister}
+								/>
+								<label htmlFor="toggle-toa-investor">Inversor</label>
+								<input
+									onChange={handleChangeRegister}
+									value="false"
+									checked={formRegister.investor === false}
+									type="radio"
+									name="investor"
+									id="toggle-toa-entreprenour"
+								/>
+								<label htmlFor="toggle-toa-entreprenour">Emprendedor</label>
+							</div>
+						</div>
+						<button
+							type="button"
+							className="btn btn-create"
+							onClick={() => handleRegister()}
+						>
+							Crear Cuenta
+						</button>
+						<div className="info-links-register">
+							<span>
+								Al registrarte, aceptas nuestra{" "}
+								<a href="#">Política de privacidad</a>,{" "}
+								<a href="#">Politica de cookies</a> y los{" "}
+								<a href="#">Términos de uso</a>
+							</span>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div className="errores-container">{errorRegister}</div>
+		</>
+	)
+}
 
-export default RegisterForm;
+export default RegisterForm
